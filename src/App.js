492a46065cb111332ffcdcb508c8abb0da5f17cb
vunguyenkhanh <ec2-user@ip-172-31-82-190.ec2.internal>
2021-04-13 01:44:25 +0000

    fix somgthing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import NavBar from "./components/layout/NavBar";
 import 'antd/dist/antd.css';
 import { Layout, Menu, Carousel, Avatar, Row, Col, Tag } from 'antd';
 
-
 function App() {
   const exampleContent1 = ["If you’re thinking of starting your very own blog, but just don’t have a clue on what to blog about, then fear not!"];
   const exampleContent2 = ["If you’re thinking of starting your very own blog, but just don’t have a clue on what to blog about, then fear not!"];
@@ -18,10 +17,10 @@ function App() {
   const [filterTagList, setFilterTagList] = useState(["All"]);
   const [tagList, setTagList] = useState(["All","React","Blockchain","PHP","BrSE","AI"])
   const [userInfo, setUserInfo] = useState({
-    name: "Duy Nguyen",
+    name: "Group 2",
     age: 18,
-    position: "BrSE",
-    company: "Google Inc.",
+    position: "Student",
+    company: "Hanoi University of Science & Technology",
     technology: "Python, NodeJS, ReactJS",
     hobby: "Reading Books, Travel"
   })
@@ -31,28 +30,28 @@ function App() {
     {
       id: 1,
       title: "Master AI in 2 hours",
-      dateCreate: new Date('December 17, 2018 03:24:00').toLocaleString('ja-JP',dateFormat),
+      dateCreate: new Date('April 11, 2021 03:24:00').toLocaleString('ja-JP',dateFormat),
       content: exampleContent1,
       selectedTag: ["AI"],
     },
     {
       id: 2,
       title: "Become BrSE - A bright future for developer",
-      dateCreate: new Date('January 14, 2020 09:40:00').toLocaleString('ja-JP',dateFormat),
+      dateCreate: new Date('April 12, 2021 09:40:00').toLocaleString('ja-JP',dateFormat),
       content: exampleContent2,
       selectedTag: ["React","BrSE"],
     },
     {
       id: 3,
       title: "PHP in a nutshell",
-      dateCreate: new Date('February 17, 2020 08:24:00').toLocaleString('ja-JP',dateFormat),
+      dateCreate: new Date('April 13, 2021 08:24:00').toLocaleString('ja-JP',dateFormat),
       content: exampleContent3,
       selectedTag: ["Blockchain","PHP"],
     },
     {
       id: 4,
       title: "Deep Learning for beginner",
-      dateCreate: new Date('March 11, 2020 09:14:00').toLocaleString('ja-JP',dateFormat),
+      dateCreate: new Date('April 14, 2021 09:14:00').toLocaleString('ja-JP',dateFormat),
       content: exampleContent4,
       selectedTag: ["AI","BrSE"],
     },
@@ -102,11 +101,11 @@ function App() {
           <>
           <Carousel style={{ marginTop: 50 }} >
             <div>
-            <img style={{width: '100%'}} src="https://images.pexels.com/photos/739349/pexels-photo-739349.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+            <img style={{width: '100%'}} src="https://en.hust.edu.vn/documents/112004/459957/20171206_About+HUST+banner.jpg/31bb1e81-595c-4c7e-bd9d-b794b2c87222?t=1558585094278"></img>
             </div>
           </Carousel>
           <div style={{ display: 'inline-flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Avatar size={200} src='https://avatars1.githubusercontent.com/u/60763388?s=460&u=1011ca50a99ede598aef7569fc793ce1e4d6f5ce&v=4' style={{display: 'inline-block', verticalAlign: 'middle', marginTop: '-100px', border: '2px solid white'}}/>
+            <Avatar size={200} src='https://pbs.twimg.com/profile_images/920013284991889416/MGr9GMmX.jpg' style={{display: 'inline-block', verticalAlign: 'middle', marginTop: '-100px', border: '2px solid white'}}/>
           </div>
           <div className="ml-auto mr-auto text-center col-md-6">
             <h1>{userInfo.name}</h1>
@@ -128,7 +127,7 @@ function App() {
           </>
           : null
         }
-        <Footer style={{ textAlign: 'center' }}>Group6 ©2020 ITSS</Footer>
+        <Footer style={{ textAlign: 'center' }}>Group 2 ©2022 ITSS</Footer>
       </Layout>
   );
 }
